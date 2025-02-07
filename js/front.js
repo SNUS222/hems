8b0da206be07f2f989b5c41251a68f1c91c78d6a
SNUS222 <osnovnoyp@gmail.com>
2025-02-07 17:25:45 +0500

    Delete js directory

--- js/front.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-    $(document).ready(function () {
-        $('.pq').click(function () {
-            $('html, body').animate({scrollTop: $("#question_" + $(this).data('q')).offset().top - 70}, 300)
-        })
-    })
-})(jQuery);
